48045aeeb3d04fd147e38e09ebf78676bc0a102b
An Bi <ab@onpu.ua>
2013-03-20 13:15:26 +0200

    Add Assign type. Add type constraints into template.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <vector>
 #include <string>
 #include <cassert>
+#include <utility>
 #include <iostream>
 #include <stdexcept>
 #include <boost/ref.hpp>
 #include <boost/bind.hpp>
+#include <boost/mpl/set.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/static_assert.hpp>
 
 // ====================== simple.ml ==================
 // exception Hell
@@ -113,7 +116,6 @@ public:
 //======================================================================
 class Var : public Exp {
    string name_;
-
 public:
    Var(string name) : name_ (name) { }
 
@@ -137,18 +139,25 @@ public:
 };
 
 //======================================================================
-//assert(e1->getInt() == (e1.get() ->* &Exp::getInt)());
 template<typename Operation, typename RetType, typename ValueAccessor,
 						const char* OpString>
 class Binop : public Exp {
+   // Available types for RetType in `class Binop'
+   typedef oo::mpl::set<
+      pair<plus<int>,Int>,   pair<multiplies<int>,Int>, pair<minus<int>,Int>,
+      pair<divides<int>,Int>, pair<less<int>,Bool>,     pair<equal_to<int>,Bool>
+      > BinopConstraints;
+   BOOST_STATIC_ASSERT((oo::mpl::has_key<BinopConstraints,
+			pair<Operation, RetType> >::value));
+
    oo::shared_ptr<const Exp> e1_;
    oo::shared_ptr<const Exp> e2_;
    ValueAccessor va;
    Operation op;
 
    // Кривизна, но что делать??? Следующий компиллер на С++ будет красивее...
-   Binop(oo::shared_ptr<const Exp> e1, oo::shared_ptr<const Exp> e2)
-      : e1_(e1), e2_(e2) { }
+   Binop(oo::shared_ptr<const Exp> e1,
+	 oo::shared_ptr<const Exp> e2) : e1_(e1), e2_(e2) { }
 public:
    Binop(const Exp* e1, const Exp* e2) : e1_(e1), e2_(e2) { }
 
@@ -180,7 +189,6 @@ class If : public Exp {
    oo::shared_ptr<const Exp> cond_;
    oo::shared_ptr<const Exp> true_;
    oo::shared_ptr<const Exp> false_;
-
 public:
    If(const Exp* c, const Exp* t, const Exp* f) :
       cond_(c), true_(t), false_(f) { }
@@ -231,14 +239,16 @@ public:
 class Seq : public Exp {
    oo::shared_ptr<const Exp> e1_;
    oo::shared_ptr<const Exp> e2_;
-   Seq(oo::shared_ptr<const Exp> e1, oo::shared_ptr<const Exp> e2)
-      : e1_(e1), e2_(e2) { }
+   Seq(oo::shared_ptr<const Exp> e1,
+       oo::shared_ptr<const Exp> e2) : e1_(e1), e2_(e2) { }
 public:
    Seq(const Exp* e1, const Exp* e2) : e1_(e1), e2_(e2) { }
 
    oo::shared_ptr<const Exp> eval(vector<Context>& c) const
    {
-      if (e1_->isSkip())
+      // @todo: think about to remove isSkip() from the code, is this just the
+      // same as isSimplistic()??? isSimplistic() is XXX, isn't it???
+      if (e1_->isSkip() || e1_->isSimplistic())
 	 return e2_;
 
       oo::shared_ptr<const Exp> e1 = e1_;
@@ -256,6 +266,41 @@ public:
 };
 
 //======================================================================
+class Assign : public Exp {
+   string name_;
+   oo::shared_ptr<const Exp> e_;
+   Assign(const string& name, oo::shared_ptr<const Exp> e)
+      : name_(name), e_(e) { }
+public:
+   Assign(const string& name, const Exp* e) : name_(name), e_(e) { }
+
+   oo::shared_ptr<const Exp> eval(vector<Context>& c) const
+   {
+      if (e_->isSimplistic()) {
+	 vector<Context>::iterator it =
+	    find_if(c.begin(), c.end(), oo::bind(&Context::name, _1) == name_);
+
+	 if (it == c.end())
+	    throw logic_error("Unbound variable name!"); // @todo: push back var
+
+	 it->value = e_;
+	 return oo::shared_ptr<const Exp>(new Skip());
+      }
+
+      oo::shared_ptr<const Exp> e = e_;
+      for (; !e->isSimplistic(); e = e->eval(c));
+      return oo::shared_ptr<const Exp>(new Assign(name_, e));
+   }
+
+   void print() const
+   {
+      cout <<  "(Assign "; cout << name_ << ","; e_->print(); cout <<")";
+   }
+
+   ~Assign() { }
+};
+
+//======================================================================
 struct IntValueAccessor { // @todo: bind it???
    int operator()(const Exp* e) const { return e->getInt(); }
 };
@@ -264,34 +309,37 @@ extern const char __extern_Plus[] = "Plus";
 extern const char __extern_Minus[] = "Minus";
 extern const char __extern_Times[] = "Times";
 extern const char __extern_Divide[] = "Divide";
-
-typedef Binop<plus<int>,       Int, IntValueAccessor, __extern_Plus>  Plus;
-typedef Binop<minus<int>,      Int, IntValueAccessor, __extern_Minus> Minus;
-typedef Binop<multiplies<int>, Int, IntValueAccessor, __extern_Times> Times;
-typedef Binop<divides<int>,    Int, IntValueAccessor, __extern_Divide> Divide;
-
 extern const char __extern_Less[] = "Less";
 extern const char __extern_Equal[] = "Equal";
 
-typedef Binop<less<int>,     Bool, IntValueAccessor, __extern_Less> Less;
-typedef Binop<equal_to<int>, Bool, IntValueAccessor, __extern_Equal> Equal;
+typedef Binop<plus<int>,       Int,  IntValueAccessor, __extern_Plus>   Plus;
+typedef Binop<minus<int>,      Int,  IntValueAccessor, __extern_Minus>  Minus;
+typedef Binop<multiplies<int>, Int,  IntValueAccessor, __extern_Times>  Times;
+typedef Binop<divides<int>,    Int,  IntValueAccessor, __extern_Divide> Divide;
+typedef Binop<less<int>,       Bool, IntValueAccessor, __extern_Less>   Less;
+typedef Binop<equal_to<int>,   Bool, IntValueAccessor, __extern_Equal>  Equal;
 
 //======================================================================
 int main()
 {
    try {
       vector<Context> c;
-      oo::shared_ptr<const Exp> e(new Less(new Var("c"),
-					   new Times(new Minus(new Var("a"),
-							       new Int(10)),
-						     new Plus(new Var("b"),
-							      new Int(1)))));
+      oo::shared_ptr<const Exp>
+	 e(new Seq(new Less(new Var("c"),
+			    new Times(new Minus(new Var("a"),
+						new Int(10)),
+				      new Plus(new Var("b"),
+					       new Int(1)))),
+		   new Seq(new Assign("d", new Int(10)),
+			   new Var("d"))));
+
       c.push_back(Context("a", new Int(12)));
       c.push_back(Context("b", new Int(31)));
       c.push_back(Context("c", new If(new Less(new Var("a"),
-					       new Var("b")),
-				  new Int(444),
-				  new Int(4))));
+						       new Var("b")),
+					      new Int(444),
+				      new Int(4))));
+      c.push_back(Context("d", new Int(0)));
 
       e->print(); cout << "\n";
 
@@ -300,7 +348,9 @@ int main()
 	 c[i].value->print(); cout << "\n";
       }
 
-      for (; !e->isSimplistic(); e = e->eval(c));
+      for (; !e->isSimplistic(); e = e->eval(c)) {
+	 cout << ":::::"; e->print(); cout << "\n";
+      }
       e->print(); cout << "\n";
 
    } catch (exception& e) {
@@ -313,3 +363,5 @@ int main()
 
    return 0;
 }
+
+//assert(e1->getInt() == (e1.get() ->* &Exp::getInt)());
